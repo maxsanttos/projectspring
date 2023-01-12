8770218bc2bd48a40226e7a6e19eb31bc1a6b5ad
maxsanttos <maxsanttos@hotmail.com>
2023-01-12 14:28:04 -0300

    testes

--- src/main/java/io/github/maxsanttos/course/entities/User.java
+++ src/main/java/io/github/maxsanttos/course/entities/User.java
@@ -4,9 +4,7 @@ import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
-
 public class User implements Serializable {
-
     @Serial
     private static final long serialVersionUID =1L;
     private Integer id;
