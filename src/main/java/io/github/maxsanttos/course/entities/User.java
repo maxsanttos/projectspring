4a51af69ca73c3b268a30b3a3e199f6532c3ed26
maxsanttos <maxsanttos@hotmail.com>
2023-01-12 14:52:38 -0300

    adcionando os properties

--- src/main/java/io/github/maxsanttos/course/entities/User.java
+++ src/main/java/io/github/maxsanttos/course/entities/User.java
@@ -1,12 +1,22 @@
 package io.github.maxsanttos.course.entities;
 
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID =1L;
+
+    @Id
     private Integer id;
     private String nome;
     private String email;
