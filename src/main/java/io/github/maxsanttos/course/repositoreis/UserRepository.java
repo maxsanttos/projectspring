f08534dbc39cc9781905daa1188d09bffbd08f01
maxsanttos <maxsanttos@hotmail.com>
2023-01-14 15:32:28 -0300

    JPA repository, dependency injection, database seedling

---
+++ src/main/java/io/github/maxsanttos/course/repositoreis/UserRepository.java
@@ -0,0 +1,11 @@
+package io.github.maxsanttos.course.repositoreis;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.github.maxsanttos.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User,Long>{
+    
+    
+
+}
