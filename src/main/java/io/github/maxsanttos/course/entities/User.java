a533e0418f5041c3e9ac7cbd8928c01c646b88f9
maxsanttos <maxsanttos@hotmail.com>
2023-01-14 14:18:06 -0300

    updates

--- src/main/java/io/github/maxsanttos/course/entities/User.java
+++ src/main/java/io/github/maxsanttos/course/entities/User.java
@@ -1,10 +1,9 @@
+
 package io.github.maxsanttos.course.entities;
 
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -17,6 +16,7 @@ public class User implements Serializable {
     private static final long serialVersionUID =1L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
